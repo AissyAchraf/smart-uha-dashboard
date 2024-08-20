50fb115319f9268d41599e0e9d2a1e973c827f00
a.aissy <achrafaissy1@gmail.com>
2024-08-20 13:46:14 +0200

    Update Track: add No Delivery Order Message

--- src/views/track/index.jsx
+++ src/views/track/index.jsx
@@ -9,6 +9,7 @@ import TrackInformation from "../../components/TrackInformation";
 import { DemandStates } from "../../utils/enums/DemandStates";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useNavigate } from "react-router-dom";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -29,6 +30,8 @@ export default function Track() {
     const { lang, translate } = useLang();
     const [demands, setDemands] = useState([]);
     const demandsRef = useRef(demands);
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(true);
 
     const fetchOngoingDemands = () => {
         API.getOngoingDemands()
@@ -68,12 +71,19 @@ export default function Track() {
         });
     };
 
+    const goToSendPackage = () => {
+        navigate('/sendColis');
+    }
+
     useEffect(() => {
+        setIsLoading(true);
         demandsRef.current = demands; // keep ref in sync with state
 
         const interval = setInterval(() => {
             fetchDemandsUpdates();
         }, 1000);
+
+        setIsLoading(false);
 
         // Cleanup function to clear the interval when the component unmounts
         return () => clearInterval(interval);
@@ -83,36 +93,49 @@ export default function Track() {
         <Box mx="20px" mt="30px">
             <Header sx={{ mt: '20px' }} title={translate('track.ongoing')} subtitle={translate('track.subtitle')}></Header>
 
-            <Box className="flex items-center flex-col h-screen">
-                {demands.length > 0 ?
-                    (<Swiper
-                        freeMode={true}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        modules={[FreeMode, Pagination]}
-                        className="max-w-[100%] lg:max-w-[50%]"
-                    >
-                    {demands.map((demand) => (
-                        <SwiperSlide key={demand._id} sx={{ width: '100%' }}>
-                            <Card sx={{ maxWidth: '600px', marginBottom: '80px'}} className="flex flex-col gap-6 group relative verflow-hidden cursor-pointer mx-6 my-8">
-                                <CardMedia
-                                    sx={{ height: 140 }}
-                                    image="/assets/images/smartuha.png"
-                                    title="SMART UHA"
-                                />
-                                <CardContent>
-                                    <TrackInformation key={demand._id} demand={demand} />
-                                </CardContent>
-                            </Card>
-                        </SwiperSlide>
-                    ))}
-                    </Swiper>)
-                    : (
-                        <>Aucune demande en cours</>
-                    )
-                }
-            </Box>
+            {isLoading ? (
+                <Box className="flex items-center justify-center flex-col" sx={{ mt: 10, width: '100%' }}>
+                    <>Chargement en cours ...</>
+                </Box>
+            ) : (
+                <Box className="flex items-center flex-col h-screen">
+                    {demands.length > 0 ?
+                        (<Swiper
+                            freeMode={true}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            modules={[FreeMode, Pagination]}
+                            className="max-w-[100%] lg:max-w-[50%]"
+                        >
+                            {demands.map((demand) => (
+                                <SwiperSlide key={demand._id} sx={{ width: '100%' }}>
+                                    <Card sx={{ maxWidth: '600px', marginBottom: '80px'}} className="flex flex-col gap-6 group relative verflow-hidden cursor-pointer mx-6 my-8">
+                                        <CardMedia
+                                            sx={{ height: 140 }}
+                                            image="/assets/images/smartuha.png"
+                                            title="SMART UHA"
+                                        />
+                                        <CardContent>
+                                            <TrackInformation key={demand._id} demand={demand} />
+                                        </CardContent>
+                                    </Card>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>)
+                        : (
+                            <Box className="flex items-center justify-center flex-col" sx={{ mt: 10, width: '100%' }}>
+                                <Typography variant="h3">Aucune livraison en cours</Typography>
+                                <img src="assets/illustrations/delivery-1.png" style={{ width: '450px' }}/>
+                                <Typography variant="h3">Vous n'avez actuellement aucune livraison en cours. Pourquoi ne pas envoyer un colis dès maintenant ?</Typography>
+                                <br/>
+                                <Button color="info" size="large" variant="contained" onClick={goToSendPackage}>Envoyer un colis</Button>
+                            </Box>
+                        )
+                    }
+                </Box>
+            )}
+
         </Box>
     )
 }
